3a5c63907a982d8f99db35f3e9538cfaf3b11ef8
5tuppl <onlystuppl4loops@gmail.com>
2026-04-24 22:16:43 +0500

    made swap languages button workable

--- app.py
+++ app.py
@@ -412,10 +412,6 @@ with col_swap:
             st.session_state.target_lang,
             st.session_state.src_lang,
         )
-        st.session_state.src_select, st.session_state.tgt_select = (
-            st.session_state.tgt_select,
-            st.session_state.src_select,
-        )
         st.session_state.translated_text = ""
         st.rerun()
     st.markdown('</div>', unsafe_allow_html=True)
